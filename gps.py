2715666563a2a78d5c5ca0132b143b0dc1a2c438
majaime <56524581+majaime@users.noreply.github.com>
2020-02-27 18:27:00 -0600

    Update gps.py

--- gps.py
+++ gps.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 import serial
-ser = serial.Serial( '/dev/ttyUSB0', 4800, timeout=5)
+ser = serial.Serial( '/dev/ttyUSB0', 4800, timeout=1)
 while 1:
 	line = ser.readline()
 	splitline = line.split(',')
 	if splitline[0] == '$GPGSV':
-		Heading = line#line[20]+line[21]+line[22]
+		Heading = line[20]
 		print(Heading)
-
